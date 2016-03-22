29b86eee68c2f8ef590f5775f8109346735ed71f
Oisin Prendiville <oisin@supertop.co>
2016-03-22 15:22:27 +0200

    Adds reproduction of skip fail bug
    - Build and run
    - Tap play
    - Tap skip back
    - Skip seems to succeed but player immediately reports that it has played to the end and clears out the player item

--- AVPlayerSkipFail/ViewController.m
+++ AVPlayerSkipFail/ViewController.m
@@ -8,20 +8,104 @@
 
 #import "ViewController.h"
 
+@import MediaPlayer;
+@import AVFoundation;
+
 @interface ViewController ()
+
+@property (nonatomic) AVQueuePlayer *player;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+NSString * NSStringFromCMTime(CMTime time) {
+  return [NSString stringWithFormat:@"%@", @(CMTimeGetSeconds(time))];
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+BOOL AVPlayerItemCanSeekToTime(AVPlayerItem *playerItem, CMTime time) {
+  for (NSValue *range in playerItem.seekableTimeRanges) {
+    CMTimeRange timeRange = [range CMTimeRangeValue];
+    if (CMTimeRangeContainsTime(timeRange, time)) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  NSURL *url = [[NSBundle mainBundle] URLForResource:@"3971767414857652294" withExtension:@"mp3"];
+  AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:url];
+  [playerItem seekToTime:CMTimeMake(120500, 100)];
+  
+  self.player = [[AVQueuePlayer alloc] initWithPlayerItem:playerItem];
+  
+  [[NSNotificationCenter defaultCenter] addObserver: self
+                                           selector: @selector(audioPlayerDidPlayToEnd:)
+                                               name: AVPlayerItemDidPlayToEndTimeNotification
+                                             object: nil];
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)skipByTimeInterval:(NSTimeInterval)interval
+{
+  AVPlayerItem *playerItem = self.player.currentItem;
+  NSLog(@"Current time %@", NSStringFromCMTime(playerItem.currentTime));
+  NSLog(@"Current duration %@", NSStringFromCMTime(playerItem.duration));
+  
+  CMTime seekToTime = CMTimeAdd(playerItem.currentTime, CMTimeMake(interval * 100, 100));
+  NSLog(@"Seek to time %@", NSStringFromCMTime(seekToTime));
+  
+  if (!AVPlayerItemCanSeekToTime(playerItem, seekToTime)) {
+    NSLog(@"Cannot seek, out of range");
+  } else {
+    if (playerItem.status == AVPlayerItemStatusReadyToPlay) {
+      [playerItem seekToTime:seekToTime completionHandler:^(BOOL finished) {
+        if (finished) {
+          NSLog(@"successfully seeked to time %@",NSStringFromCMTime(playerItem.currentTime));
+        } else {
+          NSLog(@"failed to seek to time");
+        }
+      }];
+    } else {
+      [playerItem seekToTime:seekToTime];
+    }
+  }
+}
+
+- (IBAction)togglePlayPause:(id)sender
+{
+  if (self.player.rate > 0.0f) {
+    [self.player pause];
+  } else {
+    [self.player play];
+    NSLog(@"Current time %@", NSStringFromCMTime(self.player.currentItem.currentTime));
+    NSLog(@"Current duration %@", NSStringFromCMTime(self.player.currentItem.duration));
+  }
+}
+
+- (IBAction)skipBack:(id)sender
+{
+  [self skipByTimeInterval:-15.0f];
+}
+
+- (IBAction)skipForward:(id)sender
+{
+  [self skipByTimeInterval:30.0f];
+}
+
+- (void)audioPlayerDidPlayToEnd:(NSNotification *)notification
+{
+  NSLog(@"played to end of file %@",notification);
+  NSLog(@"Current time %@", NSStringFromCMTime(self.player.currentItem.currentTime));
+  NSLog(@"Current duration %@", NSStringFromCMTime(self.player.currentItem.duration));
 }
 
 @end
