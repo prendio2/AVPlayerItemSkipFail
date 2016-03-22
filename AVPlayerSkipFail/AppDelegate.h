20903576fdf9e15975cb6b2f4393facd5ad51b64
Oisin Prendiville <oisin@supertop.co>
2016-03-22 13:41:44 +0200

    Initial Commit

---
+++ AVPlayerSkipFail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayerSkipFail
+//
+//  Created by Oisin Prendiville on 22/03/2016.
+//  Copyright © 2016 Oisin Prendiville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayerSkipFail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayerSkipFail
+//
+//  Created by Oisin Prendiville on 22/03/2016.
+//  Copyright © 2016 Oisin Prendiville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
